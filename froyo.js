1468a109792fc9fb3180e9d97f36b8ad3dd7deb3
michael morgan <mkmorgan21@protonmail.com>
2024-01-10 22:26:22 -0600

    pushing new files to branch

---
+++ froyo.js
@@ -0,0 +1,12 @@
+// * USER STORY COPIED
+
+// A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors. 
+// They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. 
+// When they view the browser console, they observe a table listing how many of each flavor they have ordered. 
+// In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo.
+
+
+
+// 1. Ask the user to type a list of froyo flavors, seperating each with a comma.
+// 2. They must type "vanilla", "vanilla", "strawberry", "coffee", and "coffee".
+// 3. Arrange prompted responses in a table. Table should have each flavor they have ordered.  
